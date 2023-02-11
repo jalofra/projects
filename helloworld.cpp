06bda9eeae0a0c341e90e266a08c906f43ca5236
jalofra <albertolofra@gmail.com>
2023-02-11 12:21:47 -0800

    Initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+        #include <iostream>
+using namespace std;
+
+
+  int waterCups;
+
+  waterCups = 2;
+
+  cout << "You should use ";
+  cout << waterCups;
+  cout << " for every cup of rice" << endl;
+
+  return 0;
+
+        
+}
